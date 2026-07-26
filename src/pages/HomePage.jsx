7af419af9e6fa3ad39a626860ae2ad072d88e96c
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-07-26 22:41:09 +0200

    lydeffekt tilføjet

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState /*useRef*/ } from "react";
+import { useEffect, useState, useRef } from "react";
 import "../css/homepage.css";
-//import bell from "../assets/sounds/bell.mp3";
+import bell from "../assets/sounds/bell.mp3";
 
 export default function HomePage() {
-  const START_TIME = 300;
+  const START_TIME = 300; //5 min
 
   const [time, setTime] = useState(START_TIME);
   const [running, setRunning] = useState(false);
-
-  //const audioRef = useRef(new Audio(bell));
+  const audioRef = useRef(new Audio(bell));
 
   useEffect(() => {
     if (!running) return;
@@ -18,7 +17,7 @@ export default function HomePage() {
         if (prev <= 1) {
           clearInterval(timer);
           setRunning(false);
-          //audioRef.current.play();
+          audioRef.current.play();
           return 0;
         }
 
